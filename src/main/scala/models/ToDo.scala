6310474a456e4f2d344efb1e2946127868d8506e
konrad <wudkowski@gmail.com>
2016-04-09 13:04:38 +0100

    added model

---
+++ src/main/scala/models/ToDo.scala
@@ -0,0 +1,9 @@
+package models
+
+import java.time.ZonedDateTime
+import java.util.UUID
+
+case class ToDo(id: UUID,
+                title: String,
+                description: String,
+                dueDate: ZonedDateTime)
